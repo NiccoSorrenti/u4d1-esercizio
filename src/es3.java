f8456bde65f7b525d8f38de2875b6055e13b6e33
Niccolò <niccolo.sorrenti@gmail.com>
2026-03-09 17:02:41 +0100

    es.3 done

--- src/es3.java
+++ src/es3.java
@@ -1,2 +1,20 @@
 public class es3 {
+    static void main() {
+        perimetroRettangolo(2, 3);
+
+        System.out.println(pariDispari(8));
+        System.out.println(pariDispari(5));
+    }
+
+    static void perimetroRettangolo(int lato1, int lato2){
+        System.out.println((lato1 + lato2) * 2);
+    }
+
+    static int pariDispari(int num){
+        return num % 2;
+    }
+
+//    static double perimetroTriangolo(double lato1, double lato2, double lato3){
+//        return;
+//    }
 }
